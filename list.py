5f2b31a47da90d56cee86c9cbe9e97c0cec4d8f0
ngowtham423-dotcom <ngowtham423@gmail.com>
2026-04-27 07:48:10 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,8 @@
+  fruit append ("orange");
+  fruit append ("grape");
+  #remove the first element
+  if "banna" in fruit:
+    fruit.remove("banana")
+# access by index
+    print(fruit[0]) 
+    # output: apple
---
+++ tuple.py
@@ -0,0 +1,7 @@
+print("\n ==TUPLE EXAMPLE==\n")
+coordinates = (10.5,20.3,10.5)
+print("tuple:", coordinates)
+print ("first coordinates:", coordinates[0])
+#tuple unpacked
+x, y, z = coordinates
+print("Unpacked values: : x=", x, "y=", y, "z=", z)
